c18276d35da602eabdd7b78a19e369a074c3d561
Tahleel1611 <133563945+Tahleel1611@users.noreply.github.com>
2025-02-14 13:35:47 +0530

    admin.py

--- inventory/admin.py
+++ inventory/admin.py
@@ -6,3 +6,7 @@ admin.site.register(Product)
 admin.site.register(Sale)
 admin.site.register(Purchase)
 admin.site.register(StockThreshold)  
+
+admin.site.site_header = "Business Dashboard "
+admin.site.site_title = "Business Dashboard"
+admin.site.index_title = "Welcome to YOur Researcher Portal"
